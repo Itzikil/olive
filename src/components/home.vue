0d26291bff1ab69269b3337f9006da7f92301268
itzhake <itzikil@icloud.com>
2026-03-11 20:29:04 +0200

    1.6

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="home" class="wide">
         <div class="home-container">
-            <video class="hero-video" autoplay muted loop playsinline controls preload="auto">
+            <video class="hero-video" autoplay muted loop playsinline preload="auto">
                 <source src="https://res.cloudinary.com/dtgejpwv9/video/upload/v1773247377/Olive/Hero%20video/hero%20video.mp4"
                     type="video/mp4" />
             </video>
