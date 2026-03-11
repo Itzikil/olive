f417a2c34d21665a6d9dfc9a200ca41d44ac2ed7
itzhake <itzikil@icloud.com>
2026-03-11 20:17:03 +0200

    bug check

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,11 @@
 <template>
     <section id="home" class="wide">
         <div class="home-container">
-            <video autoplay muted loop playsinline class="hero-video">
-                <source src="https://res.cloudinary.com/dtgejpwv9/video/upload/v1773247377/kz0j7f3bxfhxzvz1hizb.mp4"
+            <video class="hero-video" autoplay muted loop playsinline controls preload="auto">
+                <source src="https://res.cloudinary.com/dtgejpwv9/video/upload/f_auto,q_auto/kz0j7f3bxfhxzvz1hizb.mp4"
                     type="video/mp4" />
             </video>
             <h3>You o-live once</h3>
-            <!-- <img src="../assets/imgs/olive.jpg" alt="" class="logo">
-        <h2>By Emano</h2> -->
         </div>
     </section>
 </template>
