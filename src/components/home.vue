8bbcce66a91f13871c9ac51185e00ffbf6502964
itzhake <itzikil@icloud.com>
2026-03-11 20:22:03 +0200

    video bug

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,7 +2,7 @@
     <section id="home" class="wide">
         <div class="home-container">
             <video class="hero-video" autoplay muted loop playsinline controls preload="auto">
-                <source src="https://res.cloudinary.com/dtgejpwv9/video/upload/f_auto,q_auto/kz0j7f3bxfhxzvz1hizb.mp4"
+                <source src="https://res.cloudinary.com/dtgejpwv9/video/upload/v1773247377/Olive/Hero%20video/hero%20video.mp4"
                     type="video/mp4" />
             </video>
             <h3>You o-live once</h3>
