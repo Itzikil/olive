6896420200d838810746b5d6b5f69afcf3106b5e
itzhake <itzikil@icloud.com>
2026-05-04 20:10:27 +0300

    update 4

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,10 +2,13 @@
     <section id="home" class="wide">
         <div class="home-container">
             <video class="hero-video" autoplay muted loop playsinline preload="auto">
-                <source src="https://res.cloudinary.com/dtgejpwv9/video/upload/v1773247377/Olive/Hero%20video/hero%20video.mp4"
+                <source
+                    src="https://res.cloudinary.com/dtgejpwv9/video/upload/v1773247377/Olive/Hero%20video/hero%20video.mp4"
                     type="video/mp4" />
             </video>
-            <h3>You o-live once</h3>
+
+            <h3>Olive</h3>
+            <h4>You o-live once</h4>
         </div>
     </section>
 </template>
